4e464a57272412e4489fccc0a5aad357acf7e1d4
chris-yi <kri5to.yi@gmail.com>
2017-10-29 19:11:29 -0600

    initial REDUX setup complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from "react-redux";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <a href="http://localhost:8080/auth"><button>Log In</button></a>
+        {this.props.name}
       </div>
     );
   }
 }
 
-export default App;
+// export default App;
+
+function mapStateToProps(state){
+  return {
+    name: state.name
+  }
+}
+
+export default connect(mapStateToProps, {})(App);
---
+++ src/ducks/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  name: "Chris"
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
