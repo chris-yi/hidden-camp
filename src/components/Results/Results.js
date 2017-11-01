e500bef413de073af63038caf8a0eac995335c2a
chris-yi <kri5to.yi@gmail.com>
2017-10-31 19:10:01 -0600

    got results to show up on results page

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 // import axios from "axios";
+import ResultCard from "./ResultsCard";
 import { connect } from "react-redux";
 import { getListings } from "../../ducks/reducer"
+
 
 class Results extends Component {
 
@@ -12,11 +14,27 @@ class Results extends Component {
     render() {
         const allListings = this.props.allListings
         console.log(this.props.allListings)
+
+        const resultsArr = this.props.allListings?
+        this.props.allListings.map((e, i) => {
+            return (
+                <ResultCard 
+                key={i}
+                address={e.address}
+                
+                />
+            )
+        }) : null;
+
         return(
+        <div>
             <div>
                 <h1>Results Page</h1>
                 {allListings.length ? (<h1>{allListings[0].address}</h1>) : (<i className="fa fa-cog fa-spin fa-2x fa-fw"></i>)}
+                {resultsArr}
             </div>
+            
+        </div>
         )
     }
 }
---
+++ src/components/Results/ResultsCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Results(props) {
+    return (
+        <div>
+            <ul>
+                <li>{props.address}</li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
