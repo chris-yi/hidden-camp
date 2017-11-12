66bd5619727f973dda766879166f2158b886ce0f
chris-yi <kri5to.yi@gmail.com>
2017-11-12 16:39:21 -0700

    about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,26 +4,31 @@ import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import "./About.css";
 
-
 class About extends Component {
-    render() {
-      return (
-        <div className="About_Hidden_Camp">
-            <div>
-            <Navbar/>
-            </div>
-            <div className="About_Container">
-                <div className="About_Details">
-                <h1>About</h1>
-                <p>Hidden Camp was created as a personal project while attending DevMountain.  The idea of the App is for Hosts to list their private land for camp sites.  Users that want to go on unique secluded adventures can request a booking through the site and interact with the Host.  </p>
-                </div>
-            </div>
-            <div>
-            <Footer/>
-            </div>
+  render() {
+    return (
+      <div className="About_Hidden_Camp">
+        <div>
+          <Navbar />
         </div>
-      );
-    }
+        <div className="About_Container">
+          <div className="About_Details">
+            <h1>About</h1>
+            <p>
+              Hidden Camp was created as a personal project while attending
+              DevMountain. The idea of the App is for Hosts to list their
+              private land for camp sites. Users that want to go on unique
+              secluded adventures can request a booking through the site and
+              interact with the Host.{" "}
+            </p>
+          </div>
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </div>
+    );
   }
+}
 
-  export default About;
+export default About;
